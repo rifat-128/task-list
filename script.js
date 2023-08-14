45fc3a066670208952c182c3320586f6cb815685
rifat-128 <go.rifat@gmail.com>
2023-08-14 23:38:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let form = document.querySelector('#task_form')
+let tasklist = document.querySelector('ul')
+let clearbtn = document.querySelector('#clr_task')
+let filter = document.querySelector('#task_filter')
+let taskinput = document.querySelector('#new_task')
+
