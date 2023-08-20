bc3e4ee4a2f245c68ee18b3659b441463eae3a03
rifat-128 <go.rifat@gmail.com>
2023-08-21 05:32:03 +0600

    Your commit message here

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//define UI element
 let form = document.querySelector('#task_form')
 let tasklist = document.querySelector('ul')
 let clearbtn = document.querySelector('#clr_task')
 let filter = document.querySelector('#task_filter')
 let taskinput = document.querySelector('#new_task')
 
+//define event listener
+form.addEventListener('submit', a)
+
+
+//define function
